ae48b94e711316bd751ac4b786bbd368956c20cf
jalejandrovalencia <alejandrovalencia1978@gmail.com>
2018-06-13 02:12:07 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pruebaUno es una manera de estudiar y prender como funciona git y github
---
+++ css/style.css
@@ -0,0 +1,16 @@
+ <scripts>
+     $(document) .ready(function {
+       $('.slider1') .bxSlider({
+          mode: 'fade' 
+       })
+        $('.slider2').bxSlider({
+          mode: 'fade' 
+       })
+        $('.slider3').bxSlider({
+          mode: 'fade' 
+       })      
+       })
+
+     })
+     </scripts>
+  
